1845db6a4adad9d22051c149f1678a7a113dc8bf
Nguyen Nhan <nhanb2014679@student.ctu.edu.vn>
2024-01-26 12:44:16 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -9,10 +10,21 @@ app.use(cors());
 
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
     res.json({messsage: "Welcome to contact book application."});
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
