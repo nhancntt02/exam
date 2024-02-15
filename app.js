bb5dd14098efe46ed02a4fff0f1f81cb200b50f4
Nguyen Nhan <nhanb2014679@student.ctu.edu.vn>
2024-02-15 20:23:17 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
 
-
+/*
 app.use((err, req, res, next) => {
     return res.status(error.statusCode || 500).json({
     message: error.message || "Internal Server Error",
     });
 });
-
+*/
 module.exports = app;
